e9ef62efd4355ed7be04bbae278a2ee0a95ccf42
Igor-ad <autodoctor.odessa@gmail.com>
2021-01-24 10:18:50 +0200

    refactoring class Model.php

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -7,45 +7,78 @@ use App\Utils\Config;
 use PDO;
 use PDOException;
 
+
 abstract class Model
 {
-    public static function findAll(string $table): array
+    protected static PDO $db;
+    protected string $table;
+
+    public function __construct(string $table)
     {
-        $db = self::getDb();
-        $stm = $db->query("SELECT * FROM  $table");
-        $result = $stm->fetchAll(\PDO::FETCH_ASSOC);
-        return $result ? : [];
+        $this->table = $table;
+        self::$db = $this->getDb();
     }
 
-    public static function findById(string $table, int $id): array
+    public function findAll(): array
     {
-        $db = self::getDb();
-        $stm = $db->prepare("SELECT * FROM $table WHERE `id` = ?");
+        $stm = $this->getDb()->query('SELECT * FROM ' . $this->table);
+        return $stm->fetchAll(PDO::FETCH_ASSOC) ? : [];
+    }
+
+    public function findById(int $id): array
+    {
+        $sql = sprintf('SELECT * FROM %s WHERE `id` = ?', $this->table);
+        $stm = $this->getDb()->prepare($sql);
         $stm->execute([$id]);
         $result = $stm->fetch(\PDO::FETCH_ASSOC);
         return $result ? : [];
     }
 
-    public static function remove(string $table, int $id): void
+    public function findBy(array  $data): array
     {
-        $db = self::getDb();
-        $stm = $db->prepare("DELETE FROM $table WHERE `id` = ?");
+        $placeholders = $this->getKeyStr($data);
+        $sql = sprintf('SELECT * FROM %s WHERE' . $placeholders, $this->table);
+        $stm = $this->getDb()->prepare($sql);
+        $stm->execute([implode(',', $data)]);
+        $result = $stm->fetch(\PDO::FETCH_ASSOC);
+        return $result ? : [];
+    }
+
+    public function keyStr($key): string
+    {
+        $str = ' `' . $key . '` = ?';
+        return $str;
+    }
+
+    public function getKeyStr(array $data): string
+    {
+        $keyArray = array_keys($data);
+        $sqlParamStr = array_map(array($this, 'keyStr'), $keyArray);
+        return implode(' AND ', $sqlParamStr);
+    }
+
+    public function remove(int $id): void
+    {
+        $sql = sprintf('DELETE FROM %s WHERE `id` = ?', $this->table);
+        $stm = $this->getDb()->prepare($sql);
         $stm->execute([$id]);
     }
 
-    protected static function getDb()
+    public function getDb(): PDO
     {
-        try {
-            $dsn = sprintf(
-                'mysql:host=%s;dbname=%s',
-                Config::get('DB_HOST'),
-                Config::get('DB_NAME')
-            );
-            $pdo = new PDO($dsn, Config::get('DB_USER'), Config::get('DB_PASS'));
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $exception) {
-            exit('Connection to database failed');
+        if (!isset(self::$db)) {
+            try {
+                $dsn = sprintf(
+                    'mysql:host=%s;dbname=%s',
+                    Config::get('DB_HOST'),
+                    Config::get('DB_NAME')
+                );
+                self::$db = new PDO($dsn, Config::get('DB_USER'), Config::get('DB_PASS'));
+                self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $exception) {
+                exit('Connection to database failed');
+            }
         }
-        return $pdo;
+        return self::$db;
     }
 }
