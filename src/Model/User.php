8de13d1dd4043f42b8be3023378012da482ec55d
Igor-ad <autodoctor.odessa@gmail.com>
2021-01-28 18:10:13 +0200

    added check email before save/update in class src/Model/User.php

--- src/Model/User.php
+++ src/Model/User.php
@@ -8,37 +8,63 @@ class User extends Model
     public const STATUS_ACTIVE = 1;
     public const STATUS_INACTIVE = 0;
 
-    public function save($data): void
+    public function save(array $data): array
     {
-        $db = $this->getDb();
-        $stm = $db->prepare('
+        $search = $this->checkEmail($data);
+        if (!empty($search)) {
+                return $this->alertMsg($search['email']);
+        } else {
+            $db = $this->getDb();
+            $stm = $db->prepare('
             INSERT INTO ' . $this->table . ' (`name`, `email`, `password`, `status`, `created_at`)
             VALUE (?,?,?,?,now())
         ');
 
-        $stm->execute([
-            $data['name'],
-            $data['email'],
-            $data['password'],
-            self::STATUS_ACTIVE
-        ]);
+            $stm->execute([
+                $data['name'],
+                $data['email'],
+                $data['password'],
+                self::STATUS_ACTIVE
+            ]);
+            return [];
+        }
     }
 
-    public function update($data): void
+    public function update(array $data): array
     {
-        $db = $this->getDb();
-        $stm = $db->prepare('
+        $search = $this->checkEmail($data);
+        if (!empty($search) && ($data['id'] != $search['id'])) {
+            return $this->alertMsg($search['email']);
+        } else {
+            $db = $this->getDb();
+            $stm = $db->prepare('
             UPDATE ' . $this->table . '
             SET `name` = ?, `email` = ?, `password` = ?, `status` = ?
             WHERE `id` = ?
         ');
 
-        $stm->execute([
-            $data['name'],
-            $data['email'],
-            $data['password'],
-            $data['status'],
-            $data['id']
-        ]);
+            $stm->execute([
+                $data['name'],
+                $data['email'],
+                $data['password'],
+                $data['status'],
+                $data['id']
+            ]);
+        }
+        return [];
+    }
+
+    public function checkEmail(array $data): array
+    {
+        $search = ($this->findBy(['email' => $data['email']]))
+            ? $this->findBy(['email' => $data['email']]) : [];
+
+        return $search;
+    }
+
+    public function alertMsg($email): array
+    {
+        $msg[] = 'Email must be different than ' . $email;
+        return ['email' => $msg];
     }
 }
